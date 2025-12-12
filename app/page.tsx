b131b0fd9b0c591a2ab79f0603eb7eaba4518fca
Dan Navfield <dannavfield@Dans-MacBook-Pro.local>
2025-12-12 11:15:32 +1100

    refactor: migrate homepage to use storyblok content

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
-import { Hero } from '@/components/home/Hero'
-import { HowItWorks } from '@/components/home/HowItWorks'
-import { AudienceSplit } from '@/components/home/AudienceSplit'
-import { IngredientSafety } from '@/components/home/IngredientSafety'
-import { FeatureHighlights } from '@/components/home/FeatureHighlights'
-import { MissionTestimonials } from '@/components/home/MissionTestimonials'
-import { FinalCTA } from '@/components/home/FinalCTA'
+import { getStoryblokApi } from "@/lib/storyblok";
+import StoryblokPage from "@/components/StoryblokPage";
 
-export default function Home() {
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  const storyblokApi = getStoryblokApi();
+  // Fetch the home story
+  let { data } = await storyblokApi.get("cdn/stories/home", { version: "draft" });
+
   return (
-    <main className="flex min-h-screen flex-col">
-      <Hero />
-      <HowItWorks />
-      <AudienceSplit />
-      <IngredientSafety />
-      <FeatureHighlights />
-      <MissionTestimonials />
-      <FinalCTA />
-    </main>
+    <div>
+      <StoryblokPage story={data.story} />
+    </div>
   );
 }
