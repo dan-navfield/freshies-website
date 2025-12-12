1eecb272552cf305e3ad6af2211366dee5f7239d
Dan Navfield <dannavfield@Dans-MacBook-Pro.local>
2025-12-12 11:28:25 +1100

    chore: add debug indicators to page component

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,8 +1,9 @@
 import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
 
 const Page = ({ blok }: { blok: any }) => (
-    <main {...storyblokEditable(blok)} className="flex min-h-screen flex-col">
-        {blok.body.map((nestedBlok: any) => (
+    <main {...storyblokEditable(blok)} className="flex min-h-screen flex-col border-4 border-red-500 min-h-[500px]">
+        <div className="bg-red-100 p-2 text-red-600 font-bold">Internal Debug: Page Component Mounted. {blok.body?.length || 0} blocks.</div>
+        {blok.body?.map((nestedBlok: any) => (
             <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
     </main>
